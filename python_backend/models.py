23b5dc4cf41225cfc18759adf5051b7520895235
Samuel Ting <samuelting@Samuels-MacBook-Pro.local>
2025-07-29 09:00:34 +0800

    chore: commented unused code

--- python_backend/models.py
+++ python_backend/models.py
@@ -21,5 +21,5 @@ class HealthResponse(BaseModel):
     status: str
     message: str
 
-class AddIssueResponse(BaseModel):
-    message: str
+# class AddIssueResponse(BaseModel):
+#     message: str
